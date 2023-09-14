d22ca0793530f0aae749a15b3b5aaac7480fce02
Dowglas Maia <dowglasmaia@gmail.com>
2023-09-14 08:54:12 -0300

    first commit

---
+++ src/test/java/br/com/maia/webfluxapi/WebfluxApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.maia.webfluxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
